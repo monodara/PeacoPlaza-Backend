52518e0220ff036b2bf1244bec28e3f4158b8c0f
Yuan_Lu <642649909@qq.com>
2024-05-15 12:00:03 +0300

    Fix resource-based authorization for addresses.

--- Server.Infrastructure/src/Middleware/VerifyResourceOwner.cs
+++ Server.Infrastructure/src/Middleware/VerifyResourceOwner.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Logging;
 using Server.Core.src.Entity;
+using Server.Service.src.DTO;
 
 namespace Server.Infrastructure.src.Middleware
 {
@@ -17,34 +17,18 @@ namespace Server.Infrastructure.src.Middleware
         }
     }
 
-    public class VerifyResourceOwnerHandler : AuthorizationHandler<VerifyResourceOwnerRequirement, Address>
+    public class VerifyResourceOwnerHandler : AuthorizationHandler<VerifyResourceOwnerRequirement, AddressReadDto>
     {
-        private readonly ILogger<VerifyResourceOwnerHandler> _logger;
-
-        public VerifyResourceOwnerHandler(ILogger<VerifyResourceOwnerHandler> logger)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, VerifyResourceOwnerRequirement requirement, AddressReadDto resource)
         {
-            _logger = logger;
-        }
-
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, VerifyResourceOwnerRequirement requirement, Address resource)
-        {
-            _logger.LogInformation("HandleRequirementAsync is called.");
-
             var claims = context.User.Claims;
             var userId = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value; // id of authenticated user
-            _logger.LogInformation($"User ID: {userId}");
-
-            if (userId == resource.UserId.ToString())
+            if(userId == resource.UserId.ToString())
             {
-                _logger.LogInformation("Resource owner is verified.");
                 context.Succeed(requirement);
             }
-            else
-            {
-                _logger.LogInformation("Resource owner verification failed.");
-            }
-
             return Task.CompletedTask;
         }
     }
+
 }
